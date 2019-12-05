b99e77cd68b585f3c090c92c0c0f12246f95911e
RiverRobins <rivergrahamrobins@gmail.com>
2019-12-05 10:23:18 -0600

    code

---
+++ grades/Student.java
@@ -0,0 +1,26 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+    public void Student(String name){
+        this.name = name;
+        grades.clear();
+    }
+    public String getName(){
+        return this.name;
+    };
+    public void addGrade(int grade){
+        grades.add(grade);
+    };
+    public double getGradeAverage(){
+        double tot = 0;
+        for (int item : this.grades){
+            tot += (double) tot;
+        }
+        return tot / (double) this.grades.size();
+    };
+
+}
